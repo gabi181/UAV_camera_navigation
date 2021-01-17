98995615f3f8c0f0e6f6c2c78f9d1f4890779c63
Zlotnick <zelyakim@amazon.com>
2021-01-17 18:05:39 +0200

    files to project

---
+++ sim_func.py
@@ -0,0 +1,17 @@
+import random
+def create_images(im):
+    med_im_width = 1000
+    med_im_height = 1000
+    small_im_width = 400
+    small_im_height = 400
+    x_cor_med = random.randint(0,im.shape[0]-med_im_width)
+    y_cor_med = random.randint(0,im.shape[1]-med_im_height)
+
+    x_cor_small = random.randint(x_cor_med,x_cor_med+med_im_width)
+    y_cor_small = random.randint(y_cor_med,y_cor_med+med_im_height)
+
+    med_im = im[x_cor_med:x_cor_med+med_im_width,y_cor_med:y_cor_med+med_im_height,:]
+    small_im = im[x_cor_small:x_cor_small+small_im_width,y_cor_small:y_cor_small+small_im_height,:]
+    med_im_cor = [x_cor_med,y_cor_med]
+    small_im_cor = [x_cor_small,y_cor_small]
+    return [med_im,small_im,med_im_cor,small_im_cor]
