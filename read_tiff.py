e283c7a01650d7103e82350ae0b2fe40050e2994
Gabi Mannes <mannes@campus.technion.ac.il>
2021-01-19 12:33:51 +0200

    added find raod function in sim_func

--- read_tiff.py
+++ read_tiff.py
@@ -1,15 +1,19 @@
-import gdal
+# import gdal
 import matplotlib.pyplot as plt
 import numpy as np
 import tifffile
-import sim_func
+from sim_func import *
 from scipy import ndimage
+from pathlib import Path
 
-raster_path = "wetransfer-e55797/ecw1.tif"
+p = Path('.')
+raster_path = p.resolve().parent / 'data' / 'ecw1.tif'
+
 im = tifffile.imread(raster_path)[:][:][1:]
 im = np.array(im)
-
-images = sim_func.create_images(im)
+#%%
+road_im = return_road_image(im)
+images = create_images(road_im)
 small_im = images[1]
 #small_im_rotated = ndimage.rotate(small_im, 45)
 big_im = images[0]
@@ -21,24 +25,5 @@ plt.figure(2)
 plt.imshow(big_im)
 plt.show()
 
+#%%
 
-#raster_dataset = gdal.OpenEx(raster_path, gdal.GA_ReadOnly)
-#geo_transform = raster_dataset.GetGeoTransform()
-#proj = raster_dataset.GetProjectionRef()
-
-#bands_data = []
-#for b in range(1, raster_dataset.RasterCount+1):
-#    band = raster_dataset.GetRasterBand(b)
-#    bands_data.append(band.ReadAsArray())
-
-#im = gdal.Open(raster_path).ReadAsArray()
-#print(im.shape)
-
-#rgb = np.zeros((im.shape[1],im.shape[2], 3))
-#rgb[:,:,0] = im[1]
-#rgb[:,:,1] = im[2]
-#rgb[:,:,2] = im[3] #see secondincluded image for output
-
-
-#plt.imshow((rgb * 255).astype(np.uint8))
-#plt.show()
--- sim_func.py
+++ sim_func.py
@@ -1,4 +1,6 @@
 import random
+
+
 def create_images(im):
     med_im_width = 1000
     med_im_height = 1000
@@ -15,3 +17,14 @@ def create_images(im):
     med_im_cor = [x_cor_med,y_cor_med]
     small_im_cor = [x_cor_small,y_cor_small]
     return [med_im,small_im,med_im_cor,small_im_cor]
+
+
+def return_road_image(im):
+    # these specific coordinates and size fits to Newe Shaanan gate - Kikar Havectorim.
+    med_im_width = 4000
+    med_im_height = 1000
+    x_cor_med = 2000
+    y_cor_med = 4200
+    road_im = im[y_cor_med:y_cor_med + med_im_height, x_cor_med:x_cor_med + med_im_width, :]
+    return road_im
+
